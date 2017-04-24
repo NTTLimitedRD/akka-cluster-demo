afff3772f045971c834d6afbb4210e6218577382
Adam Friedman <tintoy@tintoy.io>
2017-04-24 19:44:29 +1000

    Port ConfigBuilder from DD.Cloud.Platform.Akka.

---
+++ src/ClusterDemo.Actors/ConfigBuilder.cs
@@ -0,0 +1,93 @@
+﻿using Akka.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClusterDemo.Actors
+{
+    /// <summary>
+    ///		Builds an Akka <see cref="Config"/> from a <see cref="Dictionary{TKey,TValue}"/>.
+    /// </summary>
+    public sealed class ConfigBuilder
+    {
+        /// <summary>
+        ///		Settings for serialising values into JSON (to be used as HOCON literals).
+        /// </summary>
+        static readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings
+        {
+            Converters =
+            {
+                new StringEnumConverter()
+            },
+            Formatting = Formatting.None
+        };
+
+        /// <summary>
+        ///		The underlying configuration entries.
+        /// </summary>
+        readonly Dictionary<string, object> _entries = new Dictionary<string, object>();
+
+        /// <summary>
+        ///		Create a new <see cref="ConfigBuilder"/>.
+        /// </summary>
+        public ConfigBuilder()
+        {
+        }
+
+        /// <summary>
+        ///		The underlying configuration entries.
+        /// </summary>
+        public Dictionary<string, object> Entries => _entries;
+
+        /// <summary>
+        ///		Build an Akka <see cref="Config"/> from the underlying configuration entries.
+        /// </summary>
+        /// <returns>
+        ///		The <see cref="Config"/>.
+        /// </returns>
+        public Config Build()
+        {
+            return ConfigurationFactory.ParseString(
+                hocon: BuildHocon()
+            );
+        }
+
+        /// <summary>
+        ///		Build a HOCON string from the underlying configuration entries.
+        /// </summary>
+        /// <returns>
+        ///		The HOCON string.
+        /// </returns>
+        public string BuildHocon()
+        {
+            StringBuilder hoconBuilder = new StringBuilder();
+            foreach (string key in _entries.Keys)
+            {
+                string value = FormatHoconLiteral(_entries[key]);
+
+                hoconBuilder.AppendFormat("{0} = {1}\n", key, value);
+            }
+
+            return hoconBuilder.ToString();
+        }
+
+        /// <summary>
+        ///		Format the specified value as a HOCON literal.
+        /// </summary>
+        /// <param name="value">
+        ///		The value to format.
+        /// </param>
+        /// <returns>
+        ///		The value, as a string.
+        /// </returns>
+        static string FormatHoconLiteral(object value)
+        {
+            if (value == null)
+                return String.Empty;
+
+            return JsonConvert.SerializeObject(value, SerializerSettings);
+        }
+    }
+}
---
+++ src/ClusterDemo.Actors/ConfigBuilderExtensions.cs
@@ -0,0 +1,119 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+
+using AkkaLogLevel = Akka.Event.LogLevel;
+
+namespace ClusterDemo.Actors
+{
+    /// <summary>
+    ///		Extension methods for <see cref="ConfigBuilder"/>.
+    /// </summary>
+    public static class ConfigBuilderExtensions
+    {
+        /// <summary>
+        ///		Set the system log level.
+        /// </summary>
+        /// <param name="configBuilder">
+        ///		The configuration builder.
+        /// </param>
+        /// <param name="level">
+        ///		The target log level.
+        /// </param>
+        /// <returns>
+        ///		The configuration builder (enables method chaining).
+        /// </returns>
+        public static ConfigBuilder SetLogLevel(this ConfigBuilder configBuilder, AkkaLogLevel level)
+        {
+            if (configBuilder == null)
+                throw new ArgumentNullException(nameof(configBuilder));
+
+            string logLevelValue;
+            switch (level)
+            {
+                case AkkaLogLevel.DebugLevel:
+                {
+                    logLevelValue = "debug";
+
+                    break;
+                }
+                case AkkaLogLevel.InfoLevel:
+                {
+                    logLevelValue = "info";
+
+                    break;
+                }
+                case AkkaLogLevel.WarningLevel:
+                {
+                    logLevelValue = "warning";
+
+                    break;
+                }
+                case AkkaLogLevel.ErrorLevel:
+                {
+                    logLevelValue = "error";
+
+                    break;
+                }
+                default:
+                {
+                    throw new ArgumentOutOfRangeException(nameof(level), level, "Invalid log level.");
+                }
+            }
+
+            configBuilder.Entries["akka.loglevel"] = logLevelValue;
+
+            return configBuilder;
+        }
+
+        /// <summary>
+        ///		Add a logger to the configuration.
+        /// </summary>
+        /// <typeparam name="TLogger">
+        ///		The type of logger to add.
+        /// </typeparam>
+        /// <param name="configBuilder">
+        ///		The configuration builder.
+        /// </param>
+        /// <returns>
+        ///		The configuration builder (enables method chaining).
+        /// </returns>
+        public static ConfigBuilder AddLogger<TLogger>(this ConfigBuilder configBuilder)
+            where TLogger : ActorBase
+        {
+            return configBuilder.AddLogger(typeof(TLogger));
+        }
+
+        /// <summary>
+        ///		Add a logger to the configuration.
+        /// </summary>
+        /// <param name="configBuilder">
+        ///		The configuration builder.
+        /// </param>
+        /// <param name="loggerType">
+        ///		The type of logger to add.
+        /// </param>
+        /// <returns>
+        ///		The configuration builder (enables method chaining).
+        /// </returns>
+        public static ConfigBuilder AddLogger(this ConfigBuilder configBuilder, Type loggerType)
+        {
+            if (configBuilder == null)
+                throw new ArgumentNullException(nameof(configBuilder));
+
+            if (loggerType == null)
+                throw new ArgumentNullException(nameof(loggerType));
+
+            object value;
+            List<string> loggerTypes;
+            if (!configBuilder.Entries.TryGetValue("akka.loggers", out value) || (loggerTypes = value as List<string>) == null)
+                configBuilder.Entries["akka.loggers"] = loggerTypes = new List<string>();
+
+            string loggerTypeName = $"{loggerType.FullName}, {loggerType.Assembly.GetName().Name}";
+            if (!loggerTypes.Contains(loggerTypeName))
+                loggerTypes.Add(loggerTypeName);
+
+            return configBuilder;
+        }
+    }
+}
